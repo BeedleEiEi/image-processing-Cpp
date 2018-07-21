bf98d43efe6fecc1a316186cdc4a5a69cd10a684
BeedleEiEi <58070023@kmitl.ac.th>
2018-07-21 14:58:38 +0700

    Refactored code to components and added comments for function

--- Reading Video and Camera/Reading_video_camera/main.cpp
+++ Reading Video and Camera/Reading_video_camera/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+
 using namespace std;
 
 //OpenCV Library
@@ -8,14 +9,83 @@ using namespace std;
 #include <opencv2/highgui/highgui.hpp>
 using namespace cv;
 
+class ReadingVideoCamera{
 
-//Command line Parser
+public:
+    /* This function will read video from input if video is valid,
+     * Otherwise This function will call ReadCamera function instead for open camera
+     * Expected input <String videoFilePath>
+    */
+
+    void ReadVideoCamera(String videoFile){
+
+        //  Define video capture
+        VideoCapture videoCapture;
+
+        // Check video file is exist
+        if( videoFile != "" ){
+            videoCapture.open( videoFile );
+        }
+        // Open Camera if video file doesn't exist
+        else {
+            ReadCamera( videoCapture );
+        }
+
+    }
+
+private:
+    /*  This function will open camera
+     *  Expected input <VideoCapture videoCaptureObj>
+    */
+
+    int ReadCamera(VideoCapture videoCapture){
+
+        videoCapture.open(0);
+
+        // Check camera status if available
+        if( !videoCapture.isOpened() ){
+            return -1;
+        }
+
+        CreateWindowForReadVideoOrCamera(videoCapture);
+
+        return 0;
+    }
+
+private:
+    /*  This function will create Window for show video or camera input
+     *  Expected input <VideoCapture videoCaptureObjToShow>
+    */
+
+    int CreateWindowForReadVideoOrCamera(VideoCapture videoCapture){
+
+        namedWindow( "Video", 1 );
+        for(;;){
+            Mat frame;
+            //Get a new frame from camera
+            videoCapture >> frame;
+            imshow( "Video", frame );
+            //Exit window if user press 'Exit' button
+            if( waitKey(30) >= 0 ){
+                break;
+            }
+        }
+
+        //Release camera or video after finished
+        videoCapture.release();
+        return 0;
+
+    }
+
+};
+
+
+//Command line Parsergit branch
 const char* params =
 {
     "{--help h usage ? | | print this message}"
     "{@video || Video file, if not defined try use webcamera}"
 };
-
 
 int main(int argc, const char** argv)
 {
@@ -39,35 +109,11 @@ int main(int argc, const char** argv)
 
     }
 
-    //  Define video capture
-    VideoCapture videoCapture;
+    //Define instance of class
+    ReadingVideoCamera readingVideoOrCamera;
 
-    // Check video file is exist
-    if( videoFile != "" ){
-        videoCapture.open( videoFile );
-    }
-    // Open Camera if video file doesn't exist
-    else {
-        videoCapture.open(0);
-    }
-    // Check camera status if available
-    if( !videoCapture.isOpened() ){
-        return -1;
-    }
+    //Use function to read video
+    readingVideoOrCamera.ReadVideoCamera(videoFile);
 
-    namedWindow( "Video", 1 );
-    for(;;){
-        Mat frame;
-        //Get a new frame from camera
-        videoCapture >> frame;
-        imshow( "Video", frame );
-        //Break key
-        if( waitKey(30) >= 0 ){
-            break;
-        }
-    }
-    //Release camera or video after finished
-    videoCapture.release();
-    return 0;
 
 }
