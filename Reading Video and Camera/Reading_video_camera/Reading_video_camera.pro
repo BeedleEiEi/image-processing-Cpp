54fd444bb7163cb7f9c589b5c7e4ff08c6885856
BeedleEiEi <58070023@kmitl.ac.th>
2018-07-21 13:33:12 +0700

    Created Reading video or camera

--- Reading Video and Camera/Reading_video_camera/Reading_video_camera.pro
+++ Reading Video and Camera/Reading_video_camera/Reading_video_camera.pro
@@ -4,3 +4,14 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += main.cpp
+
+INCLUDEPATH += C:/OpenCV-3.1.0/opencv/build4qt/install/include
+
+LIBS += -LC:/OpenCV-3.1.0/opencv/build4qt/install/x86/mingw/bin \
+        -lopencv_core310 \
+        -lopencv_highgui310 \
+        -lopencv_imgproc310 \
+        -lopencv_imgcodecs310 \
+        -lopencv_videoio310 \
+        -lopencv_video310 \
+        -lopencv_videostab310 \
--- Reading Video and Camera/Reading_video_camera/main.cpp
+++ Reading Video and Camera/Reading_video_camera/main.cpp
@@ -1,9 +1,73 @@
 #include <iostream>
-
+#include <string>
+#include <sstream>
 using namespace std;
 
-int main()
+//OpenCV Library
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+using namespace cv;
+
+
+//Command line Parser
+const char* params =
 {
-    cout << "Hello World!" << endl;
+    "{--help h usage ? | | print this message}"
+    "{@video || Video file, if not defined try use webcamera}"
+};
+
+
+int main(int argc, const char** argv)
+{
+    CommandLineParser parser(argc, argv, params);
+    parser.about("Read video and camera");
+
+    //  If requires help
+    if ( parser.has( "--help" ) ){
+
+        // will print parser about
+        parser.printMessage();
+        return 0;
+    }
+    String videoFile = parser.get<String>(0);
+
+    // Check if params are correctly parsed
+    if ( !parser.check() ){
+
+        parser.printErrors();
+        return 0;
+
+    }
+
+    //  Define video capture
+    VideoCapture videoCapture;
+
+    // Check video file is exist
+    if( videoFile != "" ){
+        videoCapture.open( videoFile );
+    }
+    // Open Camera if video file doesn't exist
+    else {
+        videoCapture.open(0);
+    }
+    // Check camera status if available
+    if( !videoCapture.isOpened() ){
+        return -1;
+    }
+
+    namedWindow( "Video", 1 );
+    for(;;){
+        Mat frame;
+        //Get a new frame from camera
+        videoCapture >> frame;
+        imshow( "Video", frame );
+        //Break key
+        if( waitKey(30) >= 0 ){
+            break;
+        }
+    }
+    //Release camera or video after finished
+    videoCapture.release();
     return 0;
+
 }
